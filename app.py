08376b0a510ca346e4877893e57addc9fbe2c2c4
vasp-AI <vaspaideveloper2024@gmail.com>
2024-12-14 17:55:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,8 @@ def extract_variables():
         # Use the Generative AI API to process the input
         model = genai.GenerativeModel("gemini-1.5-flash")
         prompt = (f"your are text extractor assistance ,here have input and output you need the match input and output and return the varibale for example match {input_template} and {output_text} and return variable and thier matching data\n"
-                  "i give you example how you extract the variable take the example as reference for example input is Dear Parents,{{1}} of class {{2}} is absent from {{3}} on {{4}}.Regards, Desalite Connect. and output is Dear Parents.ABHINAB of class I JASMINE Roll no. 1 is absent from SFS School on 01-04-2024.Regards, Desalite Connect then the variable is 1 is ABHINAB,2 is  I JASMINE Roll no. 1, 3 is SFS School, 4 is 01-04-2024"
+                  "i give you example how you extract the variable take the example as reference for example: input is Dear Parents,{{1}} of class {{2}} is absent from {{3}} on {{4}}.Regards, Desalite Connect. and output is Dear Parents.ABHINAB of class I JASMINE Roll no. 1 is absent from SFS School on 01-04-2024.Regards, Desalite Connect then the variable is 1 is ABHINAB,2 is  I JASMINE Roll no. 1, 3 is SFS School, 4 is 01-04-2024"
+                  "It just an example variable data will be change base on user ,so take it just an example"
                   "example format :\n"
                   "(1:data,2:data)"
                   "Do not use \n tag and also json taq on your response ,write human readble, clean")
